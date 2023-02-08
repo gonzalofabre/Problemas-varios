685062eb11b13b03d2b4f032bd59fbc2a154e15b
Gonzalo Fabre <gonzalo.fabre@outlook.es>
2023-02-08 02:19:44 -0300

    'Terminamo'

--- problema1/Completitohelados.js
+++ problema1/Completitohelados.js
@@ -1,7 +1,7 @@
 // Vamos a ver según el dinero que tengamos la lista de helados que podemos comprar
 
 //let dinero = parseFloat(prompt("Cuanto dinero tenes?"));
-let dinero = 3.5;
+let dinero = 4;
 let helados = [
     {nombre: "Helado de Vainilla", precio: 4},
     {nombre: "Helado de Chocolate", precio: 6},
@@ -14,7 +14,7 @@ let helados = [
 let listaString =   `Lista de helados: \n\n` +
 helados
 .map(function(helado) {
- return `${helado.nombre}, Precio: ${helado.precio}`
+ return `-${helado.nombre}, Precio: ${helado.precio}`
 })
 .join('\n');
 
@@ -28,56 +28,56 @@ if(heladosScope.length === 0 ) {
     console.log("No tienes suficiente dinero para comprar ningún helado")
 } else {
 
-console.log(`Tienes $${dinero}, por lo que te alcanza para los siguientes helados: ` + heladosScope
-.map((helado) =>`${helado.nombre}` )
-.join(', '));
-}
+    console.log(`Tienes $${dinero}, por lo que te alcanza para los siguientes helados: ` + heladosScope
+    .map((helado) =>`${helado.nombre}` )
+    .join(', '));
 
-let heladoMasCaro = [{nombre:"", precio:0}];
 
-heladosScope.forEach(function (helado) {
-    if(helado.precio <= dinero && heladoMasCaro[0].precio < helado.precio) {
-        heladoMasCaro = [helado] 
-    } else if(heladoMasCaro[0].precio == helado.precio) {
-        heladoMasCaro.push(helado);
+    let heladoMasCaro = [{nombre:"", precio:0}];
+
+    heladosScope.forEach(function (helado) {
+        if(helado.precio <= dinero && heladoMasCaro[0].precio < helado.precio) {
+            heladoMasCaro = [helado] 
+        } else if(heladoMasCaro[0].precio == helado.precio) {
+            heladoMasCaro.push(helado);
+        };
+
+    });
+
+    if(heladoMasCaro.length == 1) {
+        console.log('El helado más caro que puedes comprar es: '+ heladoMasCaro[0].nombre + ', cuyo precio es: ' + heladoMasCaro[0].precio);
+    }else if(heladoMasCaro.length > 1) {
+    //     console.log('Los helados más caros que puedes comprar son: \n')
+    //     for(i=0; i< heladoMasCaro.length; i++){
+    //        console.log( `-${heladoMasCaro[i].nombre}, cuyo precio es ${heladoMasCaro[i].precio} `);
+    //     };
+
+    console.log(`Los helados más caros que puedes comprar son: \n`+
+    `${heladoMasCaro.map((helado) =>'-'+ helado.nombre+ ' cuyo precio es de: '+ helado.precio).join('\n') }`)
     };
 
-});
+    let heladoMasBarato = [{nombre:"", precio: Infinity}]
 
-if(heladoMasCaro.length == 1) {
-    console.log('El helado más caro que puedes comprar es: '+ heladoMasCaro[0].nombre + ', cuyo precio es: ' + heladoMasCaro[0].precio);
-}else if(heladoMasCaro.length > 1) {
-//     console.log('Los helados más caros que puedes comprar son: \n')
-//     for(i=0; i< heladoMasCaro.length; i++){
-//        console.log( `-${heladoMasCaro[i].nombre}, cuyo precio es ${heladoMasCaro[i].precio} `);
-//     };
-
-console.log(`Los helados más caros que puedes comprar son: \n
-${heladoMasCaro.map((helado) =>'-'+ helado.nombre+ ' cuyo precio es de: '+ helado.precio).join('\n') }`)
-};
-
-let heladoMasBarato = [{nombre:"", precio: Infinity}]
-
-heladosScope.forEach(function (helado) {
-    if(helado.precio<= dinero && helado.precio< heladoMasBarato[0].precio ) {
-        heladoMasBarato = [helado]
-    }else if(helado.precio == heladoMasBarato[0].precio) {
-        heladoMasBarato.push(helado)
-    }
-})
-
-if(heladoMasBarato.length == 1) {
-    console.log(`El helado más barato que te puedes comprar es ${heladoMasBarato[0].nombre}, cuyo precio es de ${heladoMasBarato[0].precio}`);
-} else if(heladoMasBarato.length> 1 ) {
-    console.log(`Los helados más baratos que te puedes comprar son: \n
-${heladoMasBarato.map((helado) =>
-`- ${helado.nombre}, cuyo precio es de ${helado.precio}`)
-                 .join('\n')
+    heladosScope.forEach(function (helado) {
+        if(helado.precio<= dinero && helado.precio< heladoMasBarato[0].precio ) {
+            heladoMasBarato = [helado]
+        }else if(helado.precio == heladoMasBarato[0].precio) {
+            heladoMasBarato.push(helado)
         }
-    `);
-};
+    })
 
+    if(heladoMasBarato.length == 1) {
+        console.log(`El helado más barato que te puedes comprar es ${heladoMasBarato[0].nombre}, cuyo precio es de ${heladoMasBarato[0].precio}`);
+    } else if(heladoMasBarato.length> 1 ) {
+        console.log(`Los helados más baratos que te puedes comprar son: \n`+
+    `${heladoMasBarato.map((helado) =>
+    `- ${helado.nombre}, cuyo precio es de ${helado.precio}`)
+                    .join('\n')
+            }
+        `);
+    };
 
+}
 
 
 
